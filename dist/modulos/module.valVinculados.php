43f8f99cef003c22331d0e3257e6510bf59dc605
Juan Ramirez <ep.juan@gmail.com>
2017-01-28 17:34:54 -0500

    Correccion module.valVinculados.php

--- dist/modulos/module.valVinculados.php
+++ dist/modulos/module.valVinculados.php
@@ -722,7 +722,7 @@ for ($i=0;$i<sizeof($reg);$i++)
 		{
 			fwrite($txt,'1845-01-01');
 		}
-		else if (($reg[$i]["VictimaMaltrato"] == '0' || $reg[$i]["VictimaMaltrato"] == '3') && $reg[$i]["ConsultaMujerMenorVictimaInput"] == '1800-01-01')
+		else if ($reg[$i]["VictimaMaltrato"] == '21' && $reg[$i]["ConsultaMujerMenorVictimaInput"] == '1800-01-01')
 		{
 			fwrite($txt,'1845-01-01');
 		}
